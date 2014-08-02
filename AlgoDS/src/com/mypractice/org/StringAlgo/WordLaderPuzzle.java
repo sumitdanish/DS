3c0f9ae73790a47962a8f669f44095047f204299
sumitdanish <sumit.polite@gmail.com>
2014-08-03 00:07:05 +0530

    Update WordLaderPuzzle.java

--- AlgoDS/src/com/mypractice/org/StringAlgo/WordLaderPuzzle.java
+++ AlgoDS/src/com/mypractice/org/StringAlgo/WordLaderPuzzle.java
@@ -1,9 +1,38 @@
 package com.mypractice.org.StringAlgo;
 
+import java.util.HashSet;
+
 public class WordLaderPuzzle {
 
 	
+	public static void main(String[] args)
+	{
+		try
+		{
+			String startWord = "smart";
+			String endWord="roamss";
+			//String endWord;
+			WordLaderPuzzleReadDicWord wl = new WordLaderPuzzleReadDicWord("did.txt");
+			//wl.wordLaderProblem(startWord,new HashSet<String>());
+			System.out.println(wl.wordLaderLength(startWord, endWord,"did.txt"));
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
 	
+   /*
+    -----------------------------------------
+	| scart | start | swart | smalt | smarm |
+	| smart | smart | smart | smart | smart |
+	-----------------------------------------
 	
+	---------------------------------------------------------
+	| scant | scatt | scare | scarf | scarp | scars | scary |
+	| scart | scart | scart | scart | scart | scart | scart |
+	| smart | smart | smart | smart | smart | smart | smart |
+	---------------------------------------------------------
+	*/
 	
 }
