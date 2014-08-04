c7649c94701e62a12d01c626a8999cdd89d8088d
sumitdanish <sumit.polite@gmail.com>
2014-08-04 16:14:40 +0530

    Update IntervalOverLappingProblem.java

--- AlgoDS/src/com/mypractice/org/DynamicProrammingPractice/IntervalOverLappingProblem.java
+++ AlgoDS/src/com/mypractice/org/DynamicProrammingPractice/IntervalOverLappingProblem.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class IntervalOverLappingProblem {
 
@@ -13,18 +15,38 @@ public class IntervalOverLappingProblem {
 		try
 		{
 			Scanner sc = new Scanner(System.in);
-			ArrayList<Inteval> intervalList = new ArrayList<Inteval>();
+			Stack<Interval> stackIntervalList = new Stack<Interval>();
+			ArrayList<Interval> intervalList = new ArrayList<Interval>();
 			System.out.println("Enter Interval Pair..");
 			for(int i=0;i<4;i++)
 			{
 				String[] s = sc.next().split(",");
-				intervalList.add(new Inteval(Integer.parseInt(s[0]),Integer.parseInt(s[1])));
+				intervalList.add(new Interval(Integer.parseInt(s[0]),Integer.parseInt(s[1])));
 			}
 			Collections.sort(intervalList,new StartPointShorting());
-			// for(Inteval i : intervalList)
-			// {
-			// 	System.out.println("{"+i.startTime+","+i.endTime+"}");
-			// }
+			stackIntervalList.push(intervalList.get(0));
+			for(int i=1;i<intervalList.size();i++)
+			{
+				Interval in = stackIntervalList.pop();
+				if(in.endTime<intervalList.get(i).startTime)
+				{
+					stackIntervalList.push(intervalList.get(i));
+				}
+				else if(in.endTime<intervalList.get(i).endTime)
+				{
+					in.endTime=intervalList.get(i).endTime;
+					Interval in1 = stackIntervalList.pop();
+					in1.endTime=in.endTime;
+					in1.startTime=in.startTime;
+					stackIntervalList.push(in1);
+				}
+			}
+			Iterator<Interval> it = stackIntervalList.iterator();
+			while(it.hasNext())
+			{
+				Interval inte = it.next();
+				System.out.println("{"+inte.startTime+","+inte.endTime+"}");
+			}
 		}
 		catch(Exception ex)
 		{
@@ -37,21 +59,21 @@ public class IntervalOverLappingProblem {
 	
 	
 }
-class StartPointShorting implements Comparator<Inteval>
+class StartPointShorting implements Comparator<Interval>
 {
 
 	@Override
-	public int compare(Inteval o1, Inteval o2) {
+	public int compare(Interval o1, Interval o2) {
 		// TODO Auto-generated method stub
 		return o1.startTime-o2.startTime;
 	}
 	
 }
-class Inteval
+class Interval
 {
 	int startTime;
 	int endTime;
-	public Inteval(int startTime, int endTime) {
+	public Interval(int startTime, int endTime) {
 		this.startTime = startTime;
 		this.endTime = endTime;
 	}
