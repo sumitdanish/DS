ad94891806fd7486fb67a012cb0524728714a3ed
sumitdanish <sumit.polite@gmail.com>
2014-08-05 13:57:55 +0530

    Update ArrangingPatternInArray.java

--- AlgoDS/src/com/mypractice/org/ArrayPractice/ArrangingPatternInArray.java
+++ AlgoDS/src/com/mypractice/org/ArrayPractice/ArrangingPatternInArray.java
@@ -14,6 +14,40 @@ public class ArrangingPatternInArray {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		try
+		{
+			int[] a = {1,2,3,4,5,6};
+			int temp=0;
+			int[] a1 = new int[a.length];
+			for(int i=0;i<a.length;i++)
+			{
+				a1[i]=0;
+			}
+			a1[0]=a[0];
+			for(int i=0;i<a.length;i++)
+			{
+				int j=0;
+				while(j<2)
+				{
+					if(a[i+j]!=a[j+1])
+					{
+						temp=a[i+j];
+						a[i+j]=a[j+1];
+						a[j+1]=temp;
+					}
+					j++;
+				}
+			}
+			for(int i=0;i<a.length;i++)
+			{
+				System.out.print(a[i]+",");
+			}
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
 
 	}
 
