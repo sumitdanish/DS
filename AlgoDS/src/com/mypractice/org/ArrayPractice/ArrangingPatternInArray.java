c306ff9b1018296b3198f6b536a1de7dca60f866
sumitdanish <sumit.polite@gmail.com>
2014-08-05 13:44:41 +0530

    Update ArrangingPatternInArray.java

--- AlgoDS/src/com/mypractice/org/ArrayPractice/ArrangingPatternInArray.java
+++ AlgoDS/src/com/mypractice/org/ArrayPractice/ArrangingPatternInArray.java
@@ -4,6 +4,13 @@ public class ArrangingPatternInArray {
 
 	/**
 	 * @param args
+	 * Given an array convert it to another array such that the following condition holds:
+		a < b > c < d > e < f > g < h
+		where the modified array is {a,b,c,d,e,f,g,h}
+		Input:
+		1,2,3,4,5,6
+		Output:
+		1,3,2,5,4,6
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
