e6c47bfc1929012ee0547d9bf751ca2de2bd07be
sumitdanish <sumit.polite@gmail.com>
2014-08-07 17:25:03 +0530

    Update FirstNonRepetedCharecter.java

--- AlgoDS/src/com/mypractice/org/StringAlgo/FirstNonRepetedCharecter.java
+++ AlgoDS/src/com/mypractice/org/StringAlgo/FirstNonRepetedCharecter.java
@@ -6,8 +6,50 @@ public class FirstNonRepetedCharecter {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String str ="geeksftworgeeksft";
+		String[] s = new FirstNonRepetedCharecter().getFirstNonRepetedChar(str).split(",");
+		System.out.println(s[0]+","+s[1]);
 	}
 
+	
+	public String getFirstNonRepetedChar(String str)
+	{
+		try
+		{
+			int count =0;
+			char ch='p';
+			int[] arr=new int[str.length()];
+			for(int i=0;i<str.length();i++)
+			{
+				count=0;
+				for(int j=i;j<str.length();j++)
+				{
+					if(str.charAt(i)==str.charAt(j))
+					{
+						count++;
+					}
+				}
+				arr[i]=count;
+				if(count == 1)
+				{
+					ch=str.charAt(i);
+					break;
+				}
+			}
+			for(int l=0;l<arr.length;l++)
+			{
+				//System.out.println(arr[l]+","+str.charAt(l));
+			}
+			return count+","+String.valueOf(ch); 
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			//return null;
+		}
+		return null;
+	}
+	
+	
 }
