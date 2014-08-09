f6c2c4f294fa094cf7fdcc906510bb1485b655b8
sumitdanish <sumit.polite@gmail.com>
2014-08-10 05:12:04 +0530

    Update FirstNonRepetedCharecter.java

--- AlgoDS/src/com/mypractice/org/StringAlgo/FirstNonRepetedCharecter.java
+++ AlgoDS/src/com/mypractice/org/StringAlgo/FirstNonRepetedCharecter.java
@@ -7,12 +7,34 @@ public class FirstNonRepetedCharecter {
 	 */
 	public static void main(String[] args) {
 		
-		String str ="geeksftworgeeksft";
+		String str ="geeksforgeeks";
 		String[] s = new FirstNonRepetedCharecter().getFirstNonRepetedChar(str).split(",");
+		new FirstNonRepetedCharecter().indexInCharOfString(str);
 		System.out.println(s[0]+","+s[1]);
 	}
 
-	
+	private int[] indexInCharOfString(String str)
+	{
+		try
+		{
+			int[] ch = new int[256];
+			for(int i=0;i<str.length();i++)
+			{
+				//if(ch[str.charAt(i)])
+				ch[str.charAt(i)]++;
+			}
+			for(int k=0;k<ch.length;k++)
+			{
+				System.out.println(str.charAt(k)+"-->"+ch[str.charAt(k)]+",");
+			}
+			return ch;
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		return null;
+	}
 	public String getFirstNonRepetedChar(String str)
 	{
 		try
@@ -23,20 +45,20 @@ public class FirstNonRepetedCharecter {
 			for(int i=0;i<str.length();i++)
 			{
 				count=0;
-				for(int j=i;j<str.length();j++)
+				for(int j=0;j<str.length();j++)
 				{
 					if(str.charAt(i)==str.charAt(j))
 					{
 						count++;
 					}
 				}
-				arr[i]=count;
 				if(count == 1)
 				{
 					ch=str.charAt(i);
 					break;
 				}
 			}
+			
 			return count+","+String.valueOf(ch); 
 		}
 		catch(Exception ex)
